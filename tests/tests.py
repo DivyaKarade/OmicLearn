84138579c347fb2d801606af31f0f93d6d481191
furkanmtorun <furkanmtorun@gmail.com>
2020-09-14 13:02:44 +0000

    ADD some selectors for elements

--- tests/tests.py
+++ tests/tests.py
@@ -14,5 +14,15 @@ endpoint = "http://ec2-3-121-216-179.eu-central-1.compute.amazonaws.com:8501/"
 chrome.get(endpoint)
 print(chrome.title)
 
+# Define
+sample_df = chrome.find_element_by_class_name('block-container')
+print(sample_df)
+
+sample = chrome.find_element_by_xpath("//div[@class='st-cn st-dh st-dd st-cg st-bo st-bp st-bq st-br st-bt st-ax st-b0 st-c4 st-c6 st-db st-c5 st-cb st-cc st-di st-d9 st-dj st-dk']")
+print(sample)
+
+trial = chrome.find_element_by_xpath("//div[@class='Widget row-widget stSelectbox']")
+print(trial)
+
 # Quit Driver
 chrome.quit()
