8f1223615ec31c30f34e29cc1dd8ebab47cc8029
furkanmtorun <furkanmtorun@gmail.com>
2020-09-14 12:02:48 +0000

    FIX all running problems in test pipeline

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
-chrome = webdriver.Remote(
-          command_executor='http://localhost:4444/wd/hub',
-          desired_capabilities=DesiredCapabilities.CHROME)
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+chrome = webdriver.Chrome(desired_capabilities=DesiredCapabilities.CHROME, chrome_options=chrome_options)
 
 chrome.get('https://www.google.com')
 print(chrome.title)
