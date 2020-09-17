b78d4346942b8b91814c50857c68172118cfe947
furkanmtorun <furkanmtorun@gmail.com>
2020-09-17 10:53:00 +0000

    Adding GridSearchCV option to AdaBoost

--- proto_learn.py
+++ proto_learn.py
@@ -212,13 +212,15 @@ def generate_sidebar_elements(multiselect_, slider_, selectbox_, number_input_, 
 
     # Classification Methods -- Extra Parameters
     # Define variables as 0 if classifier not of those:
-    n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, \
+    tuning_type, n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, \
     solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, \
-    loss, cv_generator = 0, 0, 0, "", "", "", "", 0, 0, "", "", 0, "", 0
+    loss, cv_generator = "", 0, 0, 0, "", "", "", "", 0, 0, "", "", 0, "", 0
 
     if classifier == 'AdaBoost':
-        n_estimators = number_input_('Number of estimators:', value = 100, min_value = 1, max_value = 2000)
-        learning_rate = number_input_('Learning rate:', value = 1, min_value = 1, max_value = 100)
+        tuning_type = selectbox_('Method for tuning hyperparameters:', ["GridSearchCV", "Manual"])
+        if tuning_type == 'Manual':
+            n_estimators = number_input_('Number of estimators:', value = 100, min_value = 1, max_value = 2000)
+            learning_rate = number_input_('Learning rate:', value = 1, min_value = 1, max_value = 100)
 
     elif classifier == 'KNeighborsClassifier':
         n_neighbors = number_input_('Number of neighbors:', value = 100, min_value = 1, max_value = 2000)
@@ -271,7 +273,7 @@ def generate_sidebar_elements(multiselect_, slider_, selectbox_, number_input_, 
         features = manual_features +  additional_features
         
     return random_state, normalization, normalization_detail, n_quantiles, missing_value, feature_method, max_features, \
-        n_trees, classifiers, n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, solver, \
+        n_trees, classifiers, tuning_type, n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, solver, \
         max_iter, c_val, criterion, clf_max_features, clf_max_features_int, loss, cv_generator, cv_method, cv_splits, cv_repeats, \
         features_selected, classifier, manual_features, features
 
@@ -304,14 +306,14 @@ def feature_selection(df, option, class_0, class_1, df_sub, features, manual_fea
     return class_names, subset, X, y, features
 
 # Display results and plots
-def all_plotting_and_results(X, y, subset, cohort_column, classifier, random_state, n_estimators, learning_rate, n_neighbors, 
+def all_plotting_and_results(X, y, subset, cohort_column, classifier, random_state, tuning_type, n_estimators, learning_rate, n_neighbors, 
     knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, loss, 
     cv_generator, cv_method, cv_splits, cv_repeats, class_0, class_1):
     
     # Cross-Validation                
     st.markdown("Running Cross-Validation")
     _cv_results, roc_curve_results, pr_curve_results, split_results, y_test = \
-        perform_cross_validation(X, y, classifier, cv_method, cv_splits, cv_repeats, random_state, n_estimators, learning_rate, 
+        perform_cross_validation(X, y, classifier, cv_method, cv_splits, cv_repeats, random_state, tuning_type, n_estimators, learning_rate, 
         n_neighbors, knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, loss, cv_generator, st.progress(0))
     st.header('Cross-Validation')
 
@@ -352,7 +354,7 @@ def all_plotting_and_results(X, y, subset, cohort_column, classifier, random_sta
     if cohort_column != 'None':
         st.header('Cohort comparison')
         _cohort_results, roc_curve_results_cohort, pr_curve_results_cohort, cohort_results, cohort_combos, y_test = \
-            perform_cohort_validation(X, y, subset, cohort_column, classifier, random_state, n_estimators, learning_rate, 
+            perform_cohort_validation(X, y, subset, cohort_column, classifier, random_state, tuning_type, n_estimators, learning_rate, 
             n_neighbors, knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, loss, cv_generator, st.progress(0))
 
         # ROC-AUC for Cohorts
@@ -395,7 +397,7 @@ def all_plotting_and_results(X, y, subset, cohort_column, classifier, random_sta
 # Generate summary text
 def generate_text(normalization, normalization_detail, n_quantiles, missing_value, proteins, feature_method, max_features, n_trees, 
             classifier, cohort_column, cv_method, cv_repeats, cv_splits, class_0, class_1, summary, _cohort_results, cohort_combos,
-            n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, 
+            tuning_type, n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, 
             clf_max_features, clf_max_features_int, loss, cv_generator):
     
     st.write("## Summary")
@@ -439,7 +441,7 @@ def generate_text(normalization, normalization_detail, n_quantiles, missing_valu
 
     # Classifier
     if classifier == 'AdaBoost':
-        text += 'For classification, we used a {}-Classifier (n_estimators={} and learning_rate={}). '.format(classifier, n_estimators, learning_rate)
+        text += 'For classification, we used a {}-Classifier (n_estimators={} and learning_rate={}). '.format(classifier, tuning_type, n_estimators, learning_rate)
     elif classifier == 'LogisticRegression':
         text += 'For classification, we used a {}-Classifier (penalty={}, solver={}, max_iter={} and C={}). '.format(classifier, penalty, solver, max_iter, c_val)
     elif classifier == 'KNeighborsClassifier':
@@ -525,7 +527,7 @@ def ProtoLearn_Main():
 
     # Sidebar widgets
     random_state, normalization, normalization_detail, n_quantiles, missing_value, feature_method, max_features, n_trees, classifiers, \
-    n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, \
+    tuning_type, n_estimators, learning_rate, n_neighbors, knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, \
     loss, cv_generator, cv_method, cv_splits, cv_repeats, features_selected, classifier, manual_features, features = \
         generate_sidebar_elements(multiselect_, slider_, selectbox_, number_input_, n_missing, additional_features, proteins)
 
@@ -545,13 +547,13 @@ def ProtoLearn_Main():
 
         # Plotting and Get the results
         summary, _cohort_results, roc_curve_results_cohort, cohort_results, cohort_combos = \
-            all_plotting_and_results(X, y, subset, cohort_column, classifier, random_state, n_estimators, learning_rate, n_neighbors, 
+            all_plotting_and_results(X, y, subset, cohort_column, classifier, random_state, tuning_type, n_estimators, learning_rate, n_neighbors, 
                 knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, loss, 
                 cv_generator, cv_method, cv_splits, cv_repeats, class_0, class_1)
 
         # Generate summary text
         generate_text(normalization, normalization_detail, n_quantiles, missing_value, proteins, feature_method, max_features, n_trees, classifier, 
-            cohort_column, cv_method, cv_repeats, cv_splits, class_0, class_1, summary, _cohort_results, cohort_combos, n_estimators, learning_rate, 
+            cohort_column, cv_method, cv_repeats, cv_splits, class_0, class_1, summary, _cohort_results, cohort_combos, tuning_type, n_estimators, learning_rate, 
             n_neighbors, knn_weights, knn_algorithm, penalty, solver, max_iter, c_val, criterion, clf_max_features, clf_max_features_int, loss, cv_generator)
         
         # Session and Run info
