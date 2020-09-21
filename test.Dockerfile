e6fc73420f07d133b6dbc91ea31d817470187d3c
furkanmtorun <furkanmtorun@gmail.com>
2020-09-21 14:57:37 +0000

    REMOVE Selenium Tests on Docker since not functioning

--- test.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM selenium/standalone-chrome
-
-COPY . .
-RUN sudo chmod +x ./tests/tests_installation.sh
-RUN ./tests/tests_installation.sh
-CMD python3 ./tests/tests.py
--- tests/tests_installation.sh
+++
@@ -1,8 +0,0 @@
-# Installations for Proto Learn Testing with Selenium in Docker Container 
-
-# Basics
-sudo apt update
-sudo apt install -y python3-pip
-
-# Install selenium
-pip3 install selenium
