0236eb55b807460e3eb690a35d5ddc81e677f2b3
furkanmtorun <furkanmtorun@gmail.com>
2020-10-06 10:44:20 +0000

    ADD Alzheimer dataset into dropdown

--- proto_learn.py
+++ proto_learn.py
@@ -88,7 +88,7 @@ def main_text_and_data_upload():
     file_buffer = st.file_uploader("Upload your dataset below", type=["csv", "xlsx"])
     st.markdown("By uploading a file, you agree that you accepting [the licence agreement](https://github.com/OmicEra/proto_learn).")
     delimiter = st.selectbox("Determine the delimiter in your dataset", ["Excel File", "Comma (,)", "Semicolon (;)"])
-    sample_file = st.selectbox("Or select sample file here:", ["None", "Sample"])
+    sample_file = st.selectbox("Or select sample file here:", ["None", "Alzheimer", "Sample"])
     df = load_data(file_buffer, delimiter)
 
     return sample_file, df
