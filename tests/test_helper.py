458ecda61e3a5cfde5a0af57496061bd8f41180c
Maximilian Strauss <straussmaximilian@gmail.com>
2021-03-08 15:12:56 +0100

    FIX updataed tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -22,16 +22,16 @@ def test_load_data():
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, sheet_name='Sheet1', index=False)
     writer.save()
-    xlsx_data = load_data(output, 'Excel File')
+    xlsx_data, warnings = load_data(output, 'Excel File')
     pd.testing.assert_frame_equal(xlsx_data, df)
 
     # csv
     df = pd.DataFrame({'A': [1, 1], 'B': [0, 0]})
-    csv_data = load_data('test_csv_c.csv', 'Comma (,)')
+    csv_data, warnings = load_data('test_csv_c.csv', 'Comma (,)')
     print(csv_data)
     pd.testing.assert_frame_equal(csv_data, df)
 
-    csv_data = load_data('test_csv_sc.csv', 'Semicolon (;)')
+    csv_data, warnings = load_data('test_csv_sc.csv', 'Semicolon (;)')
     print(csv_data)
     pd.testing.assert_frame_equal(csv_data, df)
 
